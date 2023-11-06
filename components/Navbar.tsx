6ea192eac8703d8ccc02d510a7731baae87fe276
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2023-11-06 13:38:22 +0100

    Update component
    - remove redundant styles
    - make brand logo into a link
    - add customization ability for the navbar background color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,20 +3,21 @@ import Image from "next/image";
 import breach_logo from "../public/breach_logo.svg"
 import CustomLink from "./CustomLink";
 import { space_grotesk } from "@/fonts";
+import Link from "next/link";
 
 export default function Navbar({ withNavbarCTAs = false }: { withNavbarCTAs?: boolean }) {
   return (
-    <Box as='nav' bg='brand.nearWhite'>
+    <Box as='nav' bg={withNavbarCTAs ? 'brand.nearWhite' : 'white'}>
       <Flex justify='space-between' align='center' py={9} className="responsive_container">
-        <Box>
-          <Image src={breach_logo} alt='Breach logo' />
-        </Box>
+        <Link href='/'>
+          <Image src={breach_logo} alt='Breach logo' priority />
+        </Link>
         {withNavbarCTAs && 
           <HStack spacing={8}>
-            <CustomLink href="/login" variant="outline" fontWeight='medium' className={space_grotesk.className}>
+            <CustomLink href="/login" variant="outline" className={space_grotesk.className}>
               Log in
             </CustomLink>
-            <CustomLink href="/join" variant="solid" fontWeight='medium'>Join Breach</CustomLink>
+            <CustomLink href="/join" variant="solid">Join Breach</CustomLink>
           </HStack>
         }
       </Flex>
