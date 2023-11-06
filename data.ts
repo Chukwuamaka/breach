9d0c19feaf0f447a6b910ef8e265de487b4f147e
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2023-11-06 03:29:01 +0100

    Create dummy data

---
+++ data.ts
@@ -0,0 +1,59 @@
+import crypto_verse from "./public/crypto_verse.webp";
+
+export const articles = [
+  { 
+    imageSrc: crypto_verse,
+    sub_category: "The crypto verse",
+    title: "I haven’t given up on holding Bitcoin. Here’s why",
+    excerpt: "I went to boarding school and left pretty early, so I had some experience with losing friends to relocation long before the",
+    author: "Lota Anidi",
+    published_date: "12 Dec 2022"
+  },
+  { 
+    imageSrc: crypto_verse,
+    sub_category: "The crypto verse",
+    title: "I haven’t given up on holding Bitcoin. Here’s why",
+    excerpt: "I went to boarding school and left pretty early, so I had some experience with losing friends to relocation long before the",
+    author: "Lota Anidi",
+    published_date: "12 Dec 2022"
+  },
+  { 
+    imageSrc: crypto_verse,
+    sub_category: "The crypto verse",
+    title: "I haven’t given up on holding Bitcoin. Here’s why",
+    excerpt: "I went to boarding school and left pretty early, so I had some experience with losing friends to relocation long before the",
+    author: "Lota Anidi",
+    published_date: "12 Dec 2022"
+  },
+  { 
+    imageSrc: crypto_verse,
+    sub_category: "The crypto verse",
+    title: "I haven’t given up on holding Bitcoin. Here’s why",
+    excerpt: "I went to boarding school and left pretty early, so I had some experience with losing friends to relocation long before the",
+    author: "Lota Anidi",
+    published_date: "12 Dec 2022"
+  },
+  { 
+    imageSrc: crypto_verse,
+    sub_category: "The crypto verse",
+    title: "I haven’t given up on holding Bitcoin. Here’s why",
+    excerpt: "I went to boarding school and left pretty early, so I had some experience with losing friends to relocation long before the",
+    author: "Lota Anidi",
+    published_date: "12 Dec 2022"
+  },
+]
+
+export const categories = [
+  { title: 'humor', icon: '🤣' },
+  { title: 'lifehacks', icon: '💡' },
+  { title: 'photography', icon: '📸' },
+  { title: 'art', icon: '🖌' },
+  { title: 'travel', icon: '✈️' },
+  { title: 'sports', icon: '🏀' },
+  { title: 'humor', icon: '🤣' },
+  { title: 'lifehacks', icon: '💡' },
+  { title: 'photography', icon: '📸' },
+  { title: 'art', icon: '🖌' },
+  { title: 'travel', icon: '✈️' },
+  { title: 'sports', icon: '🏀' },
+]
