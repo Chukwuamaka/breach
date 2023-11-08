008562213b647df0c076c57b976214e124cd27a8
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2023-11-08 14:41:37 +0100

    Add global configuration for toast messages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme} toastOptions={{defaultOptions: { isClosable: true }}}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
