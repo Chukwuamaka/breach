6c3d860e5c619e7be31e6af2d1a53798b95ec4fa
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2023-11-08 14:40:54 +0100

    Handle click events

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -2,14 +2,22 @@ import { Button, ButtonProps } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
 interface TagProps extends ButtonProps {
+  id: string;
   selectable?: boolean;
   isSelected?: boolean;
+  handleSelect: (value: string) => void;
 }
 
-export default function Tag({ children, selectable = true, isSelected = false, ...props }: PropsWithChildren<TagProps>) {
+export default function Tag({ children, id, selectable = true, isSelected = false, handleSelect, ...props }: PropsWithChildren<TagProps>) {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!selectable) return;
+    const buttonId = (e.target as HTMLButtonElement).id;
+    handleSelect(buttonId);
+  }
+
   return (
-    <Button as={selectable ? 'button' : 'div'} variant={isSelected ? 'solid' : 'outline'}
-      size='sm' textTransform='capitalize' maxW='max-content' {...props}
+    <Button id={id} as={selectable ? 'button' : 'div'} variant={isSelected ? 'solid' : 'outline'}
+      size='sm' textTransform='capitalize' maxW='max-content' {...props} onClick={handleClick}
     >
       {children}
     </Button>
