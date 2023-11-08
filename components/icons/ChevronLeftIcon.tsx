8d91ff5de52e9b09069bb65ab1203c1d44fe2342
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2023-11-08 07:02:02 +0100

    Rename icons

---
+++ components/icons/ChevronLeftIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChevronLeftIcon({...props}) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" {...props}>
+      <path fillRule="evenodd" clipRule="evenodd" d="M10 4L6 8L10 12" stroke="#181818" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  )
+}
