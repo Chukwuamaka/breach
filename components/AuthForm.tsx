cbadc7c84aaf8b129cfd549803cf55e2a2010ad4
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2023-11-08 14:49:24 +0100

    Consume APIs for auth processes (login and register)

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -3,8 +3,10 @@ import CustomInput from "./CustomInput";
 import CustomLink from "./CustomLink";
 import useAuthForm from "@/hooks/useAuthForm";
 
-export default function AuthForm({ purpose = "signup" }: { purpose?: 'login' | 'signup' }) {
-  const { loading, formData, updateFormData, validateFormData, handleSubmit } = useAuthForm();
+export default function AuthForm({ purpose = "register" }: { purpose?: 'login' | 'register' }) {
+  const { loading, formData, updateFormData, validateFormData, handleSubmit } = useAuthForm({
+    endpoint: purpose
+  });
 
   return (
     <VStack as='form' spacing='38px' mt={12} maxW='448px' mx='auto' onSubmit={handleSubmit}>
@@ -12,20 +14,20 @@ export default function AuthForm({ purpose = "signup" }: { purpose?: 'login' | '
         value={formData.email} onChange={updateFormData}
       />
       <CustomInput input_id='password' label='Password' placeholder='Enter password'
-        value={formData.password} onChange={updateFormData}
+        type="password" value={formData.password} onChange={updateFormData}
       />
       <VStack spacing='22px' w='full'>
         <Button type='submit' w='full' bg='brand.grey.900' isLoading={loading} loadingText='Submitting...' 
-          isDisabled={validateFormData()} _disabled={{bg: 'brand.grey.300'}}
+          isDisabled={validateFormData()}
         >
           Continue
         </Button>
         <Text textStyle='sm_text' color='brand.grey.900'>
-          {purpose === "signup" ? "Already have an account" : "Don't have an account"}{'? '} 
-          <CustomLink href={`/${purpose === "signup" ? "login" : "signup"}`}
+          {purpose === "register" ? "Already have an account" : "Don't have an account"}{'? '} 
+          <CustomLink href={`/${purpose === "register" ? "login" : "register"}`}
             textDecor='underline' textStyle='sm_text' customStyles={{fontSize: '0.875rem'}}
           >
-            {purpose === "signup" ? "Log in" : "Sign up"}
+            {purpose === "register" ? "Log in" : "Sign up"}
           </CustomLink>
         </Text>
       </VStack>
--- hooks/useAuthForm.tsx
+++ hooks/useAuthForm.tsx
@@ -1,4 +1,7 @@
-import { ChangeEventHandler, useState } from "react";
+import { sendData } from "@/services/send_data.service";
+import { useToast } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { ChangeEventHandler, FormEvent, useState } from "react";
 
 interface FormDataState {
   email: string;
@@ -10,9 +13,12 @@ const initialFormData = {
   password: '',
 };
 
-export default function useAuthForm() {
+export default function useAuthForm({endpoint}: {endpoint: 'login' | 'register'}) {
   const [formData, setFormData] = useState<FormDataState>(initialFormData);
   const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
+  const makeToast = useToast();
+  const toastId = endpoint === 'login' ? 'login_toast' : 'register_toast';
 
   const updateFormData: ChangeEventHandler<HTMLInputElement> = (e) => {
     const { name, value } = e.target;
@@ -32,7 +38,50 @@ export default function useAuthForm() {
     return !validateEmail(email) || password === '';
   }
 
-  const handleSubmit = () => {}
+  const handleError = (message: string, description?: string) => {
+    if (!makeToast.isActive(toastId)) {
+      makeToast({
+        id: toastId,
+        title: message,
+        description,
+        status: 'error',
+        duration: 5000,
+      })
+    }
+  }
+
+  const handleSuccess = (data: object) => {
+    setFormData(initialFormData);
+    if (!makeToast.isActive(toastId) && endpoint === 'register') {
+      makeToast({
+        id: toastId,
+        title: 'Registration successful',
+        status: 'success',
+        duration: 3000,
+      });
+    }
+    if (endpoint === 'login') {
+      localStorage.setItem('breach_user', JSON.stringify(data))
+      router.push('/welcome');
+    } else {
+      router.push('/login');
+    }
+  }
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await sendData(`/auth/${endpoint}`, formData);
+      const data = await response.json();
+      data.error ? handleError(data.message, "Please try again.") : handleSuccess(data);
+    }
+    catch (error) {
+      handleError("An error ocurred", "Please check your internet connection or try again later.");
+    }
+    setLoading(false);
+  }
   
   return ({
     formData,
