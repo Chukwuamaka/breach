0ac996e74658fa8877f37fd27f5ac97ac964d4e5
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2023-11-08 14:39:39 +0100

    Change naming convention to avoid confusion

--- components/articles/ArticleSection.tsx
+++ components/articles/ArticleSection.tsx
@@ -1,23 +1,27 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel, TabsProps } from "@chakra-ui/react";
 import ArticleList from "./ArticleList";
 
-const categories = ["featured", "popular", "recent"];
+const labels = ["featured", "popular", "recent"];
 
-export default function ArticleSection({...props}) {
+interface ArticleSectionProps extends Omit<TabsProps, 'children'> {
+  category?: number | string;
+}
+
+export default function ArticleSection({category, ...props}: ArticleSectionProps) {
   return (
     <Tabs variant="unstyled" w='full' isLazy {...props}>
       <TabList gap={10} pb={5} px={1}>
-        {categories.map((category) => (
-          <Tab key={`tab${category}`} textStyle='md_text' pb={2} color='brand.grey.900' opacity={0.64} textTransform='capitalize'
+        {labels.map((label) => (
+          <Tab key={`tab${label}`} textStyle='md_text' pb={2} color='brand.grey.900' opacity={0.64} textTransform='capitalize'
             pl={0} pr={2} _selected={{ opacity: 1, borderBottom: '1.5px solid', borderBottomColor: 'brand.grey.900' }}
           >
-            {category}
+            {label}
           </Tab>
         ))}
       </TabList>
       <TabPanels pt={5} px={1}>
-        {categories.map((category) => (
-          <TabPanel key={`panel${category}`} px={0}>
+        {labels.map((label) => (
+          <TabPanel key={`panel${label}`} px={0}>
             <ArticleList category={category} />
           </TabPanel>
         ))}
