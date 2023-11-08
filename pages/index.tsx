c16091d33e9cebd5bf1f9eda3ecd3d6cc86cc026
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2023-11-08 07:08:30 +0100

    Make categories section a bit wider

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,8 @@ export default function Home() {
       {/* Article Section */}
       <Box as='section' bg='white'>
         <Flex justify='space-between' pt='60px' pb='200px' className="responsive_container">
-          <ArticleSection flexBasis='65.7%' />
-          <Categories flexBasis='31%' />
+          <ArticleSection flexBasis='64%' />
+          <Categories flexBasis='32.5%' />
         </Flex>
       </Box>
     </Page>
